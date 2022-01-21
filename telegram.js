49bafb8e56f67ca6e46fa332dd1a60b95c65bb72
ggulmool <kimny86@gmail.com>
2022-01-21 19:14:20 +0900

    add

---
+++ telegram.js
@@ -0,0 +1,23 @@
+const TelegramBot = require('node-telegram-bot-api')
+
+const TOKEN = '5170198483:AAHOc6Tz2sLkHnfmtmu1iZSYGWSmMJobLaM'
+
+const bot = new TelegramBot(TOKEN, { polling: true })
+const sendTelegramMessage = (chatId, message) => {
+    return bot.sendMessage(chatId, message)
+}
+
+bot.onText(/\/test (.+)/, async (msg, match) => {
+    try {
+        let msgRes = match[1]
+
+        // send message
+        await bot.sendMessage(msg.chat.id, msgRes)
+        console.log(`telegramUtil] now to:${msg.chat.id}`)
+    } catch (e) {
+        console.log('telegramUtil] pushTelegram err:', e.toString)
+        throw e
+    }
+})
+
+module.export = sendTelegramMessage
